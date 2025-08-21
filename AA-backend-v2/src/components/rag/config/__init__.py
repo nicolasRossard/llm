ef7a78416913ea42f8d75744112eecc160192fbc
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 16:09:10 +0200

    fix error name

--- AA-backend-v2/src/components/rag/config/__init__.py
+++ AA-backend-v2/src/components/rag/config/__init__.py
@@ -1,11 +1,11 @@
 from embedding_model_config import EmbeddingModelConfig
 from llm_model_config import LLMModelConfig
-from rag_config import RAGPConfig
+from rag_config import RAGConfig
 from .vector_repository_config import VectorRepositoryConfig
 
 __all__ = [
     "EmbeddingModelConfig",
     "LLMModelConfig",
-    "RAGPConfig",
+    "RAGConfig",
     "VectorRepositoryConfig"
 ]
--- AA-backend-v2/src/components/rag/config/rag_config.py
+++ AA-backend-v2/src/components/rag/config/rag_config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class RAGPConfig(BaseSettings):
+class RAGConfig(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='RAG_', extra="forbid")
     system_prompt: str
