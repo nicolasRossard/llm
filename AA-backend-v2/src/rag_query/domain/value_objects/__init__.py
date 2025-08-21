be6a5947b4378964a33d4ae2079ea03e141139fc
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 14:09:23 +0200

    fix __init__ value objects

---
+++ AA-backend-v2/src/rag_query/domain/value_objects/__init__.py
@@ -0,0 +1,17 @@
+from document_retrieval import DocumentRetrieval, DocumentRetrievalVector
+from message import Message
+from model_config import ModelConfig
+from query import Query
+from rag_config import RAGConfig
+from responses import Response, RAGResponse
+
+__all__ = [
+    "DocumentRetrieval",
+    "DocumentRetrievalVector",
+    "Message",
+    "ModelConfig",
+    "Query",
+    "RAGConfig",
+    "RAGResponse",
+    "Response"
+]
