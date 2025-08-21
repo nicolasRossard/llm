26f5d5075a932728873427351ba40619a03c89ff
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 15:28:05 +0200

    clean vo

--- AA-backend-v2/src/components/rag/domain/value_objects/__init__.py
+++ AA-backend-v2/src/components/rag/domain/value_objects/__init__.py
@@ -1,11 +1,8 @@
 from document_retrieval import DocumentRetrieval, DocumentRetrievalVector
 from input_document import InputDocument, InputDocumentResult
 from message import Message
-from model_config import ModelConfig
 from query import Query
-from rag_config import RAGConfig
 from responses import Response, RAGResponse
-from .vector_repository_config import VectorRepositoryConfig
 
 __all__ = [
     "DocumentRetrieval",
@@ -13,10 +10,7 @@ __all__ = [
     "InputDocument",
     "InputDocumentResult",
     "Message",
-    "ModelConfig",
     "Query",
-    "RAGConfig",
     "RAGResponse",
     "Response",
-    "VectorRepositoryConfig"
 ]
