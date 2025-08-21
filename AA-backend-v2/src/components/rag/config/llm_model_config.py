cee382664ec2159936cbbc52ab53e4f57bdbfb7d
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 16:05:31 +0200

    add conf llm and paremeters

--- AA-backend-v2/src/components/rag/config/llm_model_config.py
+++ AA-backend-v2/src/components/rag/config/llm_model_config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class LLMModelConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='LLM_', extra="forbid")
     model_name: str
     temperature: float = 0.2
     max_tokens: int = 2048
