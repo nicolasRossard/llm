ddf7cf44dc10347139aa43807e31c56f1f3ac180
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 16:56:26 +0200

    __init__ config added

--- AA-backend-v2/src/components/rag/config/__init__.py
+++ AA-backend-v2/src/components/rag/config/__init__.py
@@ -1,6 +1,6 @@
-from embedding_model_config import EmbeddingModelConfig
-from llm_model_config import LLMModelConfig
-from rag_config import RAGConfig
+from .embedding_model_config import EmbeddingModelConfig
+from .llm_model_config import LLMModelConfig
+from .rag_config import RAGConfig
 from .vector_repository_config import VectorRepositoryConfig
 
 __all__ = [
