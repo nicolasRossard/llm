bfc675607ff1b4a172cf3565d735ff4b5ebe6299
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-21 15:27:51 +0200

    create config folder

---
+++ AA-backend-v2/src/components/rag/domain/config/__init__.py
@@ -0,0 +1,9 @@
+from model_config import ModelConfig
+from rag_config import RAGConfig
+from .vector_repository_config import VectorRepositoryConfig
+
+__all__ = [
+    "ModelConfig",
+    "RAGConfig",
+    "VectorRepositoryConfig"
+]
