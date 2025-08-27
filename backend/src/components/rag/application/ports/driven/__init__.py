3176f0fa17d5ac51e9672e151e1d85d33dad35eb
Nicolas TWISTER Rossard <nicolas.rossard.tw@davidson.fr>
2025-08-27 23:28:33 +0200

    improve import modules

--- backend/src/components/rag/application/ports/driven/__init__.py
+++ backend/src/components/rag/application/ports/driven/__init__.py
@@ -4,12 +4,13 @@ Domain to adapters
 """
 from .embedding_port import EmbeddingPort
 from .llm_port import LLMPort
+from .text_chunking_port import TextChunkingPort
 from .vector_retriever_port import VectorRetrieverPort
 from .vector_store_port import VectorStorePort
-
 __all__ = [
     "EmbeddingPort",
     "LLMPort",
+    "TextChunkingPort",
     "VectorRetrieverPort",
     "VectorStorePort"
 ]
---
+++ backend/src/components/rag/infrastructure/adapters/driven/text_chunking/__init__.py
@@ -0,0 +1,4 @@
+"""Only display Text Chunking Adapter used in the application."""
+from .docling_text_chunking_adapter import DoclingTextChunkingAdapter
+
+__all__ = ["DoclingTextChunkingAdapter"]
